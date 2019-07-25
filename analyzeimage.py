9f13da11b03f23f7b36a9af7179281f72f88987a
Pratyush Das <42460747+das2002@users.noreply.github.com>
2019-07-25 15:27:45 -0400

    Update analyzeimage.py

--- analyzeimage.py
+++ analyzeimage.py
@@ -1,7 +1,18 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
 
 
 def analyzeimage(old_image):
 	# analyze the image 'old_image' here
+	img = cv2.imread(old_image)
+	edges = cv2.Canny(img, 100,200)
+	
+	plt.subplot(121), plt.imshow(img, cmap = 'gray')
+	plt.title('Original Image'), plt.xticks([]), plt.yticks([])
+	plt.subplot(122), plt.imshow(edges, cmap = 'gray')
+	plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
+	plt.show()
 	# save the analyzed image to 'static/temp.jpg'
 	# return the path of the new image file
 	return "static/temp.jpg"
